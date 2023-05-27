0532952bcb633495f03ba6d86168790901651690
StarrFox <StarrFox6312@gmail.com>
2023-05-27 17:35:18 +0000

    update gh-poi

--- packages/gh-poi/default.nix
+++ packages/gh-poi/default.nix
@@ -5,15 +5,16 @@
 }:
 buildGoModule rec {
   pname = "gh-poi";
-  version = "0.9.0";
+  version = "0.9.1";
 
   src = fetchFromGitHub {
     owner = "seachicken";
     repo = "gh-poi";
     rev = "v${version}";
-    sha256 = "sha256-7lvqiD0yitc3kvXLD9AtCBCp+F+fqRJyLdnio1R6oP8=";
+    sha256 = "sha256-7KZSZsYfo9zZ0HSg5yLDNTlwb30byD73kqMNHc0tQpo=";
   };
 
+  # TODO: figure out what this is
   vendorSha256 = "sha256-D/YZLwwGJWCekq9mpfCECzJyJ/xSlg7fC6leJh+e8i0=";
   doCheck = false;
 }
