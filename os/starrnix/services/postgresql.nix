aaf72b7072b9bc6d2c4b0730c54a4fd99dc3b1ab
StarrFox <StarrFox6312@gmail.com>
2023-06-01 19:27:59 +0000

    add smitestat db

--- os/starrnix/services/postgresql.nix
+++ os/starrnix/services/postgresql.nix
@@ -3,12 +3,14 @@
     enable = true;
     ensureDatabases = [
       "discord_chan"
+      "smitestat"
     ];
     ensureUsers = [
       {
         name = "starr";
         ensurePermissions = {
           "DATABASE discord_chan" = "ALL PRIVILEGES";
+          "DATABASE smitestat" = "ALL PRIVILEGES";
         };
       }
     ];
