34b44ca5cfb2a489d97d747adf1611930e90a4dc
StarrFox <StarrFox6312@gmail.com>
2023-06-06 03:49:55 +0000

    add docker and portainer modules

--- os/common/users/starr.nix
+++ os/common/users/starr.nix
@@ -5,7 +5,7 @@
   users.users.starr = {
     isNormalUser = true;
     description = "starr";
-    extraGroups = ["networkmanager" "wheel" "libvirtd" "audio"];
+    extraGroups = ["networkmanager" "wheel" "libvirtd" "audio" "docker"];
     shell = pkgs.fish;
     initialHashedPassword = "$y$j9T$FF0N8WmrfwlzpN924bHgF/$h6MQqAxuOLe1LiS.1GqOx104aUbwtyho9lnLocm4iq3";
   };
