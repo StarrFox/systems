1acc548f34c1e41141e5da49e59910f7633c4730
StarrFox <StarrFox6312@gmail.com>
2023-06-13 20:19:33 +0000

    add testing dbs to postgres

--- os/starrnix/services/postgresql.nix
+++ os/starrnix/services/postgresql.nix
@@ -3,12 +3,16 @@ _: {
     enable = true;
     ensureDatabases = [
       "smitestat"
+      "arcanumbot"
+      "discord_chan"
     ];
     ensureUsers = [
       {
         name = "starr";
         ensurePermissions = {
           "DATABASE smitestat" = "ALL PRIVILEGES";
+          "DATABASE arcanumbot" = "ALL PRIVILEGES";
+          "DATABASE discord_chan" = "ALL PRIVILEGES";
         };
       }
     ];
