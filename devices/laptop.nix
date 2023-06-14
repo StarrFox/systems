aac9daec1ba973786d54b0ff5daa5166a7fc5137
StarrFox <StarrFox6312@gmail.com>
2023-06-14 09:49:06 +0000

    update laptop device

--- devices/laptop.nix
+++ devices/laptop.nix
@@ -1,37 +1,40 @@
 {
+  config,
   lib,
   modulesPath,
   ...
 }: {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "sdhci_pci" "rtsx_usb_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/c087455e-5fc8-430b-98f0-63dc90b0f6e5";
+    device = "/dev/disk/by-uuid/f43fb9a4-7712-40dc-b799-8eeaf2f2c40f";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/0AA9-9AAC";
+    device = "/dev/disk/by-uuid/8BF5-DD74";
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/4b0061d6-2f50-465e-b8b5-3b873fe6655d";}
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-
-  services.spice-vdagentd.enable = true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   ssh-keys = import ../ssh-keys.nix;
   users = with ssh-keys; [starr-starrnix];
-  systems = with ssh-keys; [starrnix test_vm];
+  systems = with ssh-keys; [starrnix test_vm nixtop];
 in {
   # used to test if it's working
   "test.age".publicKeys = users ++ systems;
-  "discord_chan_token.age".publicKeys = users ++ systems;
-  "arcanumbot_token.age".publicKeys = users ++ systems;
+  "discord_chan_token.age".publicKeys = with ssh-keys; [starr-starrnix nixtop];
+  "arcanumbot_token.age".publicKeys = with ssh-keys; [starr-starrnix nixtop];
 }
