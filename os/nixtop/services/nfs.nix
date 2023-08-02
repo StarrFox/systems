f64d2e890e4e23a9540978cf38f75695ed6d29f0
StarrFox <StarrFox6312@gmail.com>
2023-08-02 15:59:05 +0000

    make nfs ports static and allow them through the firewall

--- os/nixtop/services/nfs.nix
+++ os/nixtop/services/nfs.nix
@@ -4,7 +4,11 @@ _: {
     exports = ''
       /nfs 192.168.1.71(rw,fsid=0,no_subtree_check)
     '';
+    statdPort = 4000;
+    lockdPort = 4001;
+    mountdPort = 4002;
+    createMountPoints = true;
   };
 
-  networking.firewall.allowedTCPPorts = [2049];
+  networking.firewall.allowedTCPPorts = [2049 4000 4001 4002];
 }
