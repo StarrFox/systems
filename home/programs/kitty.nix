8b3d4c8c598573fbbb1077d20c4439c2756eef87
StarrFox <StarrFox6312@gmail.com>
2023-11-17 11:33:48 +0000

    set kitty font name

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,6 +1,11 @@
 _: {
   programs.kitty = {
     enable = true;
-    font.size = 20;
+
+    font = {
+      # TODO: should we use font.package to readd this font?
+      name = "FiraCode Nerd Font Mono";
+      size = 20;
+    };
   };
 }
