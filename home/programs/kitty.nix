c650a0a9eef9aed9bc3366d0ace30672635c2c2b
StarrFox <StarrFox6312@gmail.com>
2023-11-17 11:43:59 +0000

    set kitty theme

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,7 +1,7 @@
 _: {
   programs.kitty = {
     enable = true;
-
+    theme = "Adwaita darker";
     font = {
       # TODO: should we use font.package to readd this font?
       name = "FiraCode Nerd Font Mono";
