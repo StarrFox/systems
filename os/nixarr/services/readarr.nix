7a55b1ea77982917400d874636bffeb8fcb765f4
StarrFox <StarrFox6312@gmail.com>
2023-11-24 06:28:18 +0000

    use unstable readarr

--- os/nixarr/services/readarr.nix
+++ os/nixarr/services/readarr.nix
@@ -1,7 +1,14 @@
-_: {
+{inputs, pkgs, config, ...}: let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "${pkgs.system}";
+    inherit (config.nixpkgs) config;
+  };
+in {
   services.readarr = {
     enable = true;
     openFirewall = true;
     group = "arr";
+    # TODO: 23.11 switch to stable
+    package = nixpkgs-unstable.readarr;
   };
 }
