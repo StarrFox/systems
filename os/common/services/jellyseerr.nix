89e7649cd4830a9f6eef09aa81e96c9f49f9339a
StarrFox <StarrFox6312@gmail.com>
2023-11-24 07:02:49 +0000

    use unstable jellyseerr

--- os/common/services/jellyseerr.nix
+++ os/common/services/jellyseerr.nix
@@ -1,6 +1,46 @@
-_: {
-  services.jellyseerr = {
-    enable = true;
-    openFirewall = true;
+{inputs, pkgs, config, ...}: let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "${pkgs.system}";
+    inherit (config.nixpkgs) config;
   };
+in {
+  # TODO: 23.11 reenable
+  # services.jellyseerr = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
+
+  # nixos module didn't have a .package so had to do this for unstable
+  systemd.services.jellyseerr = {
+    description = "Jellyseerr, a requests manager for Jellyfin";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    environment.PORT = "5055";
+    serviceConfig = {
+      Type = "exec";
+      StateDirectory = "jellyseerr";
+      WorkingDirectory = "${nixpkgs-unstable.jellyseerr}/libexec/jellyseerr/deps/jellyseerr";
+      DynamicUser = true;
+      ExecStart = "${nixpkgs-unstable.jellyseerr}/bin/jellyseerr";
+      BindPaths = [ "/var/lib/jellyseerr/:${nixpkgs-unstable.jellyseerr}/libexec/jellyseerr/deps/jellyseerr/config/" ];
+      Restart = "on-failure";
+      ProtectHome = true;
+      ProtectSystem = "strict";
+      PrivateTmp = true;
+      PrivateDevices = true;
+      ProtectHostname = true;
+      ProtectClock = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectKernelLogs = true;
+      ProtectControlGroups = true;
+      NoNewPrivileges = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      RemoveIPC = true;
+      PrivateMounts = true;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 5055 ];
 }
