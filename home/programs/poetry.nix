f5bdf06bd033f724af1cbf8e4ec4669f22c74a79
StarrFox <StarrFox6312@gmail.com>
2023-11-30 04:36:39 +0000

    writeTOML is only in unstable

--- home/programs/poetry.nix
+++ home/programs/poetry.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
+{pkgs, inputs, config, ...}: let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "${pkgs.system}";
+    inherit (config.nixpkgs) config;
+  };
+in {
+  # TODO: 23.11 unstable no longer needed
   xdg.configFile."pypoetry/config.toml" = {
     enable = true;
-    source = pkgs.writers.writeTOML "config.toml" {
+    source = nixpkgs-unstable.writers.writeTOML "config.toml" {
       virtualenvs.in-project = true;
     };
   };
