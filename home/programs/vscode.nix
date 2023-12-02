d296ad97e1bbc8f341c3701646ceba52670d1185
StarrFox <StarrFox6312@gmail.com>
2023-12-02 08:37:00 +0000

    Revert "disable declaritive vscode settings/extensions"
    This reverts commit 2dd4b875435df8d1bad1a3148c53320ae5c56ff5.

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -23,89 +23,89 @@ in  {
     enable = true;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
-    # userSettings = {
-    #   "editor.fontSize" = 17;
-    #   "editor.fontLigatures" = true;
-    #   "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monospace'";
-    #   "editor.formatOnSaveMode" = "file";
-    #   "files.autoSave" = "afterDelay";
-    #   "explorer.confirmDelete" = false;
-    #   "explorer.confirmDragAndDrop" = false;
-    #   "git.confirmSync" = false;
-    #   "git.autofetch" = true;
-    #   "window.zoomLevel" = 1;
-    #   # NOTE: this requires the pitch black theme listed in extensions
-    #   #"workbench.colorTheme" = "Pitch Black";
-    #   # NOTE: requires andromeda extension below
-    #   "workbench.colorTheme" = "Andromeda Bordered";
-    #   "workbench.startupEditor" = "none";
-    #   "python.analysis.inlayHints.functionReturnTypes" = true;
-    #   "python.analysis.inlayHints.variableTypes" = true;
-    #   # NOTE: options are off, basic, and strict
-    #   "python.analysis.typeCheckingMode" = "basic";
-    #   "python.venvPath" = "~/.cache/pypoetry/virtualenvs";
-    #   # this allows the black extension to work
-    #   "python.formatting.provider" = "none";
-    #   "python.testing.pytestEnabled" = true;
-    #   "python.terminal.activateEnvironment" = false;
-    #   "[python]" = {
-    #     "editor.formatOnSave" = true;
-    #     #"editor.formatOnType" = true;
-    #     "editor.defaultFormatter" = "ms-python.black-formatter";
-    #     "editor.codeActionsOnSave" = {
-    #       "source.organizeImports" = true;
-    #     };
-    #   };
-    #   "isort.args" = ["--profile" "black" "--skip-gitignore"];
-    #   "nix.enableLanguageServer" = true;
-    #   "nix.serverPath" = "nil";
-    #   "[nix]" = {
-    #     "editor.defaultFormatter" = "kamadorueda.alejandra";
-    #     "editor.formatOnPaste" = true;
-    #     "editor.formatOnSave" = true;
-    #     "editor.formatOnType" = true;
-    #   };
-    #   "zig.path" = "";
-    #   "zig.zls.path" = "zls";
-    #   "zig.zls.checkForUpdate" = false;
-    #   # "editor.quickSuggestions" = {
-    #   #   "other" = "inline";
-    #   # };
-    #   "python.analysis.completeFunctionParens" = true;
-    #   "python.analysis.autoFormatStrings" = true;
-    #   "python.analysis.diagnosticMode" = "workspace";
-    #   "python.analysis.inlayHints.callArgumentNames" = "all";
-    #   "lldb.suppressUpdateNotifications" = true;
-    # };
+    userSettings = {
+      "editor.fontSize" = 17;
+      "editor.fontLigatures" = true;
+      "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monospace'";
+      "editor.formatOnSaveMode" = "file";
+      "files.autoSave" = "afterDelay";
+      "explorer.confirmDelete" = false;
+      "explorer.confirmDragAndDrop" = false;
+      "git.confirmSync" = false;
+      "git.autofetch" = true;
+      "window.zoomLevel" = 1;
+      # NOTE: this requires the pitch black theme listed in extensions
+      #"workbench.colorTheme" = "Pitch Black";
+      # NOTE: requires andromeda extension below
+      "workbench.colorTheme" = "Andromeda Bordered";
+      "workbench.startupEditor" = "none";
+      "python.analysis.inlayHints.functionReturnTypes" = true;
+      "python.analysis.inlayHints.variableTypes" = true;
+      # NOTE: options are off, basic, and strict
+      "python.analysis.typeCheckingMode" = "basic";
+      "python.venvPath" = "~/.cache/pypoetry/virtualenvs";
+      # this allows the black extension to work
+      "python.formatting.provider" = "none";
+      "python.testing.pytestEnabled" = true;
+      "python.terminal.activateEnvironment" = false;
+      "[python]" = {
+        "editor.formatOnSave" = true;
+        #"editor.formatOnType" = true;
+        "editor.defaultFormatter" = "ms-python.black-formatter";
+        "editor.codeActionsOnSave" = {
+          "source.organizeImports" = true;
+        };
+      };
+      "isort.args" = ["--profile" "black" "--skip-gitignore"];
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "[nix]" = {
+        "editor.defaultFormatter" = "kamadorueda.alejandra";
+        "editor.formatOnPaste" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnType" = true;
+      };
+      "zig.path" = "";
+      "zig.zls.path" = "zls";
+      "zig.zls.checkForUpdate" = false;
+      # "editor.quickSuggestions" = {
+      #   "other" = "inline";
+      # };
+      "python.analysis.completeFunctionParens" = true;
+      "python.analysis.autoFormatStrings" = true;
+      "python.analysis.diagnosticMode" = "workspace";
+      "python.analysis.inlayHints.callArgumentNames" = "all";
+      "lldb.suppressUpdateNotifications" = true;
+    };
 
-    # extensions = with pkgs.vscode-extensions; [
-    #   njpwerner.autodocstring
-    #   tamasfe.even-better-toml
-    #   usernamehw.errorlens
-    #   davidanson.vscode-markdownlint
-    #   jnoortheen.nix-ide
-    #   ms-python.python
-    #   ms-python.vscode-pylance
-    #   mkhl.direnv
-    #   editorconfig.editorconfig
-    #   matklad.rust-analyzer
-    #   kamadorueda.alejandra
-    #   viktorqvarfordt.vscode-pitch-black-theme
-    #   skellock.just
-    #   gruntfuggly.todo-tree
-    #   github.vscode-github-actions
-    #   github.vscode-pull-request-github
-    #   mhutchie.git-graph
-    #   vadimcn.vscode-lldb
-    #   serayuzgur.crates
-    #   humao.rest-client
+    extensions = with pkgs.vscode-extensions; [
+      njpwerner.autodocstring
+      tamasfe.even-better-toml
+      usernamehw.errorlens
+      davidanson.vscode-markdownlint
+      jnoortheen.nix-ide
+      ms-python.python
+      ms-python.vscode-pylance
+      mkhl.direnv
+      editorconfig.editorconfig
+      matklad.rust-analyzer
+      kamadorueda.alejandra
+      viktorqvarfordt.vscode-pitch-black-theme
+      skellock.just
+      gruntfuggly.todo-tree
+      github.vscode-github-actions
+      github.vscode-pull-request-github
+      mhutchie.git-graph
+      vadimcn.vscode-lldb
+      serayuzgur.crates
+      humao.rest-client
 
-    #   starrpkgs.vscord
-    #   starrpkgs.vscode-zig
-    #   starrpkgs.andromeda
+      starrpkgs.vscord
+      starrpkgs.vscode-zig
+      starrpkgs.andromeda
 
-    #   nixpkgs-unstable.vscode-extensions.ms-python.isort
-    #   nixpkgs-unstable.vscode-extensions.ms-python.black-formatter
-    # ];
+      nixpkgs-unstable.vscode-extensions.ms-python.isort
+      nixpkgs-unstable.vscode-extensions.ms-python.black-formatter
+    ];
   };
 }
