1a2ad50cf2cefab789d4154138cdab7e7ada6d10
StarrFox <StarrFox6312@gmail.com>
2023-12-06 14:48:51 +0000

    gitea: use hostname for domain

--- os/common/services/gitea.nix
+++ os/common/services/gitea.nix
@@ -1,7 +1,8 @@
-_: {
+{config, ...}: {
   # TODO: use postgres for database
   services.gitea = {
     enable = true;
     dump.enable = true;
+    server.DOMAIN = config.networking.hostName;
   };
 }
