1df5c7f5e041242608ae5bf10edba21d897669f8
StarrFox <StarrFox6312@gmail.com>
2023-12-07 00:11:37 +0000

    home: disable annoying kitty popup

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -27,5 +27,7 @@
       "ctrl+c" = "copy_and_clear_or_interrupt";
       "ctrl+v" = "paste_from_clipboard";
     };
+    # disables the annoying pop-up when you close kitty
+    extraConfig = "confirm_os_window_close 0";
   };
 }
