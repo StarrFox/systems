cdd5737a5e265cabff564857cc2cd11d8817f430
StarrFox <StarrFox6312@gmail.com>
2023-12-07 21:43:46 +0000

    enviorment variables must be strings

--- os/common/roles/desktop/wayland/hyperland.nix
+++ os/common/roles/desktop/wayland/hyperland.nix
@@ -11,7 +11,7 @@ _: {
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     XDG_SESSION_TYPE = "wayland";
-    WLR_NO_HARDWARE_CURSORS = 1;
+    WLR_NO_HARDWARE_CURSORS = "1";
 
     # NOTE: these two can apparently cause issues
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
