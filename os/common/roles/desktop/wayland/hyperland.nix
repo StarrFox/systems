ad29f0c0ce0e6d9114aecb74456e853ed7e1ee60
StarrFox <StarrFox6312@gmail.com>
2023-12-08 18:45:58 +0000

    fix hyperland and switch back to plasma

--- os/common/roles/desktop/wayland/hyperland.nix
+++ os/common/roles/desktop/wayland/hyperland.nix
@@ -12,12 +12,11 @@ _: {
     LIBVA_DRIVER_NAME = "nvidia";
     XDG_SESSION_TYPE = "wayland";
     WLR_NO_HARDWARE_CURSORS = "1";
+    # electron app support
+    NIXOS_OZONE_WL = "1";
 
     # NOTE: these two can apparently cause issues
     #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
     GBM_BACKEND = "nvidia-drm";
   };
-
-  # TODO: remove
-  services.xserver.desktopManager.plasma5.enable = true;
 }
