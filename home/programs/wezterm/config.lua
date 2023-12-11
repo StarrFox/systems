c296a9d0c90788dc0b9702cb41ad0357d6e586d7
StarrFox <StarrFox6312@gmail.com>
2023-12-11 18:28:30 +0000

    home: wezterm hide tab bar

--- home/programs/wezterm/config.lua
+++ home/programs/wezterm/config.lua
@@ -15,7 +15,9 @@ end
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.font_size = 18.0 
+config.font_size = 18.0
+
+config.hide_tab_bar_if_only_one_tab = true
 
 -- For example, changing the color scheme:
 --config.color_scheme = 'AdventureTime'
