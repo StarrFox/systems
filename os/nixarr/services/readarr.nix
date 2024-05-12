e3f6e992764a8df8810a6c622864065d5acdcc6f
StarrFox <StarrFox6312@gmail.com>
2024-05-12 05:32:28 -0500

    nixarr: use unstable channel package for readarr

--- os/nixarr/services/readarr.nix
+++ os/nixarr/services/readarr.nix
@@ -1,7 +1,13 @@
-_: {
+{inputs, config, pkgs, ...}: let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "${pkgs.system}";
+    inherit (config.nixpkgs) config;
+  };
+in {
   services.readarr = {
     enable = true;
     openFirewall = true;
     group = "arr";
+    package = nixpkgs-unstable.readarr;
   };
 }
