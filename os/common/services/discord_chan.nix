fd3b2062c222f2def9e75caf6796d2e10df7bc5f
StarrFox <StarrFox6312@gmail.com>
2025-03-24 16:57:34 -0500

    update lock and set exaroton key in dc

--- os/common/services/discord_chan.nix
+++ os/common/services/discord_chan.nix
@@ -5,9 +5,16 @@
     owner = "discord_chan";
   };
 
+  age.secrets.exaroton = {
+    file = ../../../secrets/exaroton.age;
+    mode = "400";
+    owner = "discord_chan";
+  };
+
   services.discord_chan = {
     enable = true;
     tokenFile = config.age.secrets.discord_chan_token.path;
+    exarotonTokenFile = config.age.secrets.exaroton.path;
   };
 
   services.postgresql = {
