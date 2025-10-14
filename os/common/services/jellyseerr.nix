c67dab1839369739e7a185f0af11207bef557a0b
StarrFox <StarrFox6312@gmail.com>
2025-10-14 00:41:07 -0500

    enable jellyseerr on nixtop

--- os/common/services/jellyseerr.nix
+++ os/common/services/jellyseerr.nix
@@ -1,6 +1,12 @@
-_: {
+{inputs, config, pkgs, ...}: let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "${pkgs.system}";
+    inherit (config.nixpkgs) config;
+  };
+in {
   services.jellyseerr = {
     enable = true;
     openFirewall = true;
+    package = nixpkgs-unstable.jellyseerr;
   };
 }
