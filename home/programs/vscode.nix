cd0499c221bce3dcb4e353ba750a598667db9ce2
StarrFox <StarrFox6312@gmail.com>
2026-01-16 13:36:47 -0600

    switch to vscodium

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -19,6 +19,7 @@ in {
 
   programs.vscode = {
     enable = true;
+    package = pkgs.vscodium;
     profiles.default = {
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
