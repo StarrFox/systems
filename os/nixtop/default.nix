b0d3730e4f474f3447c703c0257cb353c90de9c4
StarrFox <StarrFox6312@gmail.com>
2026-02-08 22:41:53 -0600

    nixarr: disable demanding services

--- os/nixtop/default.nix
+++ os/nixtop/default.nix
@@ -15,12 +15,12 @@ in {
     ../common/services/discord_chan.nix
     ../common/services/jellyfin.nix
     #../common/services/tailscale.nix
-    ../common/services/nextcloud.nix
+    #../common/services/nextcloud.nix
     ../common/services/syncthing.nix
     ../common/services/postgresql_backup.nix
     ../common/services/jellyseerr.nix
 
-    ../common/containers/portainer.nix
+    #../common/containers/portainer.nix
 
     ../common/users/starr.nix
 
@@ -28,15 +28,14 @@ in {
 
     ../common/roles/server.nix
 
-    ./services/nfs.nix
-    ./services/nginx.nix
+    #./services/nfs.nix
+    #./services/nginx.nix
     ./services/gitea.nix
   ];
 
   users.users.starr.openssh.authorizedKeys.keys = [
     ssh-keys.starr-nixmain
     ssh-keys.nixmain
-    ssh-keys.starrwin
     ssh-keys.nixarr
   ];
 
