08dfc9442540d1a3a9543e395049f519d8f35163
StarrFox <StarrFox6312@gmail.com>
2026-02-22 05:02:25 -0600

    rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,9 @@ let
   ssh-keys = import ../ssh-keys.nix;
   # users = with ssh-keys; [starr-nixmain];
   # systems = with ssh-keys; [nixmain test_vm nixtop];
-  deployment = with ssh-keys; [starr-nixmain nixtop];
+  deployment = with ssh-keys; [starr-nixmain nixcell];
 in {
   "discord_chan_token.age".publicKeys = deployment;
-  "arcanumbot_token.age".publicKeys = deployment;
   "nextcloud_pass.age".publicKeys = deployment;
   "exaroton.age".publicKeys = deployment;
 }
