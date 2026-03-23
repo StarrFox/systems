abcec5afc17d85a286c681cf279c2a275bd5b3f1
StarrFox <StarrFox6312@gmail.com>
2026-03-23 07:14:13 -0500

    nixarr: move qbittorrent home to /media

--- os/nixarr/services/qbittorrent.nix
+++ os/nixarr/services/qbittorrent.nix
@@ -1,7 +1,12 @@
-_: {
+{config, ...}: {
   services.qbittorrent = {
     enable = true;
     group = "arr";
     openFirewall = true;
+    profileDir = "/media/downloads";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /media/downloads 0777 ${config.services.qbittorrent.user} ${config.services.qbittorrent.group} - -"
+  ];
 }
