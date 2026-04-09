d62d1e8d644de7aa301d8c47d84be0a1d138c200
StarrFox <StarrFox6312@gmail.com>
2026-04-09 02:36:56 -0500

    prism: add semeru java

--- home/package_sets/gui.nix
+++ home/package_sets/gui.nix
@@ -25,8 +25,22 @@
     # games
     # TODO: update when new jdks come out
     # check https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/pr/prismlauncher/package.nix#L37-L42
-    (prismlauncher.override {jdks = [graalvmPackages.graalvm-ce jdk25 jdk21 jdk17 jdk8];})
-    #mangohud
+    (prismlauncher.override {
+      jdks = [
+        graalvmPackages.graalvm-ce
+        javaPackages.compiler.semeru-bin.jre-21
+        javaPackages.compiler.semeru-bin.jre-17
+        javaPackages.compiler.semeru-bin.jre-8
+        jdk25
+        jdk21
+        jdk17
+        jdk8
+      ];
+    })
+    # TODO: prism wants these on path
+    mangohud
+    gamemode
+
     heroic
     pcsx2
     the-powder-toy
