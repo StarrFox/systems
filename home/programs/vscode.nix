73329677fab4e3e72c5aa8878257cfd16af697bb
StarrFox <StarrFox6312@gmail.com>
2026-07-20 13:08:17 -0400

    vscode: add claude

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -142,6 +142,9 @@ in
         ms-vscode.makefile-tools
         ziglang.vscode-zig
 
+        # AI (work)
+        anthropic.claude-code
+
         starrpkgs.vscord
         starrpkgs.vscode-coconut
         starrpkgs.vscode-lark
