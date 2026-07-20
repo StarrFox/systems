441780aafbadc981dc7ce73b0caa1350393853eb
StarrFox <StarrFox6312@gmail.com>
2026-07-20 10:48:41 -0400

    remove optional parameter

--- os/common/services/ollama.nix
+++ os/common/services/ollama.nix
@@ -1,8 +1,8 @@
-{pkgs, use_cuda ? true, ...}: {
+{pkgs, ...}: {
   services.ollama = {
     enable = true;
     # Optional: preload models, see https://ollama.com/library
     loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
-    package = if use_cuda then pkgs.ollama-cuda else pkgs.ollama-cpu;
+    package = pkgs.ollama-cuda;
   };
 }
